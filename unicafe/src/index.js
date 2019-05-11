425cd8ca483b6555d399b753a76428f3a199fa27
Sanaubia <jprokkola@gmail.com>
2019-05-11 14:58:30 +0300

    unicafe 1.6

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -7,9 +7,35 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const goodClick = () => {
+    setGood(good+ 1)
+  }
+
+  const neutralClick = () => {
+    setNeutral(neutral+ 1)
+  }
+
+  const badClick = () => {
+    setBad(bad+ 1)
+  }
+
   return (
+    
     <div>
-      code here
+    
+    <h1>Anna palautetta</h1>
+        
+
+    <button style ={{marginRight: 10}} onClick ={goodClick}>Good</button>
+      <button style ={{marginRight: 10}} onClick ={neutralClick}>Neutral</button>
+      <button onClick ={badClick}>Bad</button>
+      
+
+      <h1>Statistiikka</h1>
+        
+            <p>Hyvä {good}</p>
+            <p>Neutraali {neutral} </p>
+            <p>Huono {bad} </p>
     </div>
   )
 }
